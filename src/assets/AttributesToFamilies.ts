f675b132351a638915921994c5104779414d2748
visor841 <izylstra@gmail.com>
2021-05-24 17:30:55 -0400

    Add some more attributes

--- src/assets/AttributesToFamilies.ts
+++ src/assets/AttributesToFamilies.ts
@@ -4,5 +4,33 @@ export const AttributesToFamiliesDictionary: AttributeFamilyValue[] = [
     {
         attribute: "achieve",
         creator: 1
+    },
+    {
+        attribute: "action",
+        citizen: 1,
+        explorer: 1,
+        hero: 1,
+        jester: 1,
+        rebel: 1
+    },
+    {
+        attribute: "awake",
+        jester: 1
+    },
+    {
+        attribute: "beauty",
+        lover: 1
+    },
+    {
+        attribute: "belief",
+        caregiver: 1,
+        creator: 1,
+        innocent: 1,
+        magician: 1,
+        rebel: 1
+    },
+    {
+        attribute: "challenge",
+        hero: 1
     }
 ]
