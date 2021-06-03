abbac729d0933be23cd4e99621aa07ff61d37f87
visor841 <izylstra@gmail.com>
2021-06-03 16:10:17 -0400

    Add logic for family subset of question list

--- src/app/services/question.service.ts
+++ src/app/services/question.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { QuestionDictionary } from 'src/assets/QuestionList';
+import { AttributesToFamiliesDictionary } from '../../assets/AttributesToFamilies';
 import { Question } from '../interface';
 
 @Injectable({
@@ -12,13 +13,38 @@ export class QuestionService {
   private usedQuestionList: Array<Question>
   private whichQuestionList = "full";
   constructor() {
-    this.usedQuestionList = this.createQuestionList("full");
+    this.usedQuestionList = this.createQuestionList("family");
     this.question$ = new BehaviorSubject(this.getQuestion(this.usedQuestionList));
   }
   createQuestionList(whichList): Array<Question> {
+    let newQuestionList: Array<Question>
     switch(whichList) {
       case "full":
         return this.fullQuestionList;
+      case "family":
+        let famAttr: Array<String>
+        let qAdded = false;
+        AttributesToFamiliesDictionary.forEach((famCon) => {
+          famAttr.push(famCon.attribute);
+        })
+        this.fullQuestionList.forEach((question) => {
+          question.forEach((key) => {
+            if(this.isAttributeKey(key)) {
+              famAttr.forEach((attribute) => {
+                if(key == attribute) {
+                  newQuestionList.push(question);
+                  qAdded = true;
+                  return;
+                }
+              })
+            }
+            if(qAdded == true) {
+              qAdded = false;
+              return;
+            }
+          })
+        })
+        return newQuestionList;
     }
   }
   getQuestion(questionList): Question {
