0224a9ca3f3f06defb3d9d09c870d91eb2d98eec
visor841 <izylstra@gmail.com>
2021-06-03 13:00:15 -0400

    Setup for family reports
    Figure out which family is most identified with the user, and properly set up "no data entered" and "more data needed" states for the report page

--- src/app/pages/report/report.page.ts
+++ src/app/pages/report/report.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FamilyReportDisplay } from 'src/app/interface';
+import { UserService } from 'src/app/services/user.service';
+import { FamilyReports } from 'src/assets/FamilyReports';
 
 @Component({
   selector: 'app-report',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./report.page.scss'],
 })
 export class ReportPage implements OnInit {
+  public maxFamily: string;
+  public FamilyReportDisplay: FamilyReportDisplay;
+  private familyReports = FamilyReports;
 
-  constructor() { }
+  constructor(private uService: UserService) {
+    this.uService.maxFamily$.subscribe((val) =>{
+      this.maxFamily = val;
+      this.generateFamilyReport();
+    })
+  }
 
   ngOnInit() {
+  }
+
+  generateFamilyReport() {
+    if (this.maxFamily == "tie") {
+      return;
+    }
+
+    this.FamilyReportDisplay = this.familyReports[this.maxFamily];
   }
 
 }
